12ab34eb5a9e40ee95e2b93e3aae74b54bd0690b
178inaba <178inaba@users.noreply.github.com>
2017-07-05 15:06:37 +0900

    First commit

---
+++ HelloWorld-macOSApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld-macOSApp
+//
+//  Created by 178inaba on 2017/07/05.
+//  Copyright (c) 2017 178inaba. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
